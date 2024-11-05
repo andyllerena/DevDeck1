f221d59d87e4e225c3f2f34fd13db8274930321e
Andy Llerena <andyllerena28@gmail.com>
2024-11-04 21:36:58 -0500

    bug fix

--- components/ProgressBox.tsx
+++ components/ProgressBox.tsx
@@ -1,7 +1,7 @@
 // components/ProgressBox.tsx
 'use client';
 import React from 'react';
-import DoughnutChart from './DoughnutChart';
+import DoughnutChart from '../components/DoughnutChart';
 
 import { useProgress } from './ProgressContent';
 const StatItem = ({
